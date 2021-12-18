50f35c49e2fe0bb132a254fab76d03f9f173d424
Michel Villalba <richard.villalba.mendieta@gmail.com>
2021-12-17 22:14:00 -0400

    loading y primera parte

---
+++ js/index1.js
@@ -0,0 +1,9 @@
+function atractivoGruta(){
+    location.href="https://www.youtube.com/watch?v=3EjqJV7IZqo";
+}
+function atractivoLaguna(){
+    location.href="https://www.youtube.com/watch?v=bR6xhCh7EtA";
+}
+function atractivoPlaza(){
+    location.href="https://www.youtube.com/watch?v=K0Lg0e4axVI";
+}
